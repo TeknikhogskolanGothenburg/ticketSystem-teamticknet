7dec548a363ea9137faccd89b9641a1399f8cbcc
Kalle Blomnell <Karlmikaelb@gmail.com>
2018-02-07 11:30:26 +0100

    Added a controller for our API

--- src/Restapi_Webapplication/Startup.cs
+++ src/Restapi_Webapplication/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -35,6 +36,13 @@ namespace Restapi_Webapplication
             }
 
             app.UseMvc();
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("MVC didn't find anything!");
+            });
+
+            
         }
     }
 }
