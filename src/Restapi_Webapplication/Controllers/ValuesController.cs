6e27016be1584f40c22c7cff6f5b927e02926280
Morgan Berglund <herr.berglund@gmail.com>
2018-02-08 09:31:49 +0100

    just usless change of number

--- src/Restapi_Webapplication/Controllers/ValuesController.cs
+++ src/Restapi_Webapplication/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace Restapi_Webapplication.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value" };
         }
 
         // GET api/values/5
