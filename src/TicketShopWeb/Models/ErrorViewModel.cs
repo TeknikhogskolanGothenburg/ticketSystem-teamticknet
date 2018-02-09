eccc1e571fb914ee54e8b525edfcb2299969a71e
Kalle Blomnell <Karlmikaelb@gmail.com>
2018-02-09 11:13:14 +0100

    Added webapplication to Ticketshop.sln

---
+++ src/TicketShopWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TicketShopWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
