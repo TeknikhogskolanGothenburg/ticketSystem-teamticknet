36d695c80977be7edf3198060ecaf577a1ac6cae
Kalle Blomnell <Karlmikaelb@gmail.com>
2018-02-13 09:50:31 +0100

    Fixed connectionstrings

--- src/Restapi_Webapplication/Controllers/VenuesPostController.cs
+++ src/Restapi_Webapplication/Controllers/VenuesPostController.cs
@@ -8,16 +8,17 @@ using TicketSystem.DatabaseRepository.Model;
 
 namespace Restapi_Webapplication.Controllers
 {
-    [Produces("application/json")]
-    [Route("api/[controller]")]
-    public class VenuesPostController : Controller
-    {
-		TicketDatabase database = new TicketDatabase();
-		//POST api/Venues
-		[HttpPost]
-			 public void Post([FromBody]string value)
-		{
-			return database.VenueAdd(Request.Form["Vname"],  )
-		 }
-}
+    //[Produces("application/json")]
+    //[Route("api/[controller]")]
+    //public class VenuesPostController : Controller
+    //{
+    //    TicketDatabase database = new TicketDatabase();
+    //    //POST api/Venues
+    //    [HttpPost]
+    //    public void Post([FromBody]string value)
+    //    {
+    //        return database.VenueAdd(Request.Form["Vname"],  )
+
+    //     }
+    //}
 }
