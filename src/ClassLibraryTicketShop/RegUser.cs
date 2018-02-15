33c927ba32b359827b063d941e3c2332174d7117
Morgan Berglund <herr.berglund@gmail.com>
2018-02-15 11:11:17 +0100

    Reguser added with new public RegUser Class

---
+++ src/ClassLibraryTicketShop/RegUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibraryTicketShop
+{
+    public class RegUser
+    {
+        
+            public int ID { get; set; }
+            public string FName { get; set; }
+            public string LName { get; set; }
+            public string Password { get; set; }
+            public string City { get; set; }
+        
+    }
+}
