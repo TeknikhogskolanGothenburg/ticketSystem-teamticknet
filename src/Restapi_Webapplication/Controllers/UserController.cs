5d685b7922ec2ebca63a153bf4673c960c42586b
Morgan Berglund <herr.berglund@gmail.com>
2018-02-15 13:18:43 +0100

    reguser changes // GET: api/User/Get {query}

--- src/Restapi_Webapplication/Controllers/UserController.cs
+++ src/Restapi_Webapplication/Controllers/UserController.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using TicketSystem.DatabaseRepository;
+using ClassLibraryTicketShop;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Restapi_Webapplication.Controllers
 {
@@ -11,39 +15,40 @@ namespace Restapi_Webapplication.Controllers
     [Route("api/User")]
     public class UserController : Controller
     {
+
         // GET: api/User/Get
+        TicketDatabase getUser = new TicketDatabase();
         [Route("Get")]
-        [HttpGet]
-        public IEnumerable<string> Get()
+        [HttpGet("{query}")]
+        public List<UserReg> Get(string query)
         {
-            return new string[] { "value1", "value2" };
+            return getUser.UserRegFind(query);
         }
+        //// GET: api/User/5
+        //[HttpGet("{id}", Name = "Get")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+        
+        //// POST: api/User
+        //[HttpPost]
+        //public void Post([FromBody]string value)
+        //{
+        //}
+        
+        //// PUT: api/User/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+        
+        //// DELETE: api/ApiWithActions/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
 
-        // GET: api/User/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-        
-        // POST: api/User
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-        
-        // PUT: api/User/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-        
-        // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-
-        }
+        //}
     }
 }
 
