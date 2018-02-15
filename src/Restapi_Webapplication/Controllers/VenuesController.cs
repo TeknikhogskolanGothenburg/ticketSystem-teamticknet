f533931d05dafb83216ece7ae39e26328d328b34
Grasuggan <32665572+Grasuggan@users.noreply.github.com>
2018-02-15 09:51:36 +0100

    VenuesController Added using NewtonJason Added Post method

--- src/Restapi_Webapplication/Controllers/VenuesController.cs
+++ src/Restapi_Webapplication/Controllers/VenuesController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using TicketSystem.DatabaseRepository;
 using ClassLibraryTicketShop;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Restapi_Webapplication.Controllers
 {
@@ -13,12 +15,14 @@ namespace Restapi_Webapplication.Controllers
 	[Route("api/Venues")]
 	public class VenuesController : ControllerBase
 	{
-  //      // GET api/Venues
-  //      [HttpGet]
+		//      // GET api/Venues
+		//      [HttpGet]
 		//public IEnumerable<string> Get()
 		//{
 		//	return new string[] { "value1", "value2" };
 		//}
+
+		TicketDatabase database = new TicketDatabase;
 
 		//Get api/Venues
 		[HttpGet("{query}")]
@@ -27,26 +31,35 @@ namespace Restapi_Webapplication.Controllers
 			return TicketDatabase.VenuesSpecific(query);
 		}
 
-		
+		[HttpPost]
+		public void Post([FromBody]JObject data)
+		{
+			Venue venue = data["Venue"].ToObject<Venue>();
+			database.VenueAdd(venue.VenueName, venue.Address, venue.City, venue.Country);
 
 
-   //     // POST api/Venues
-   //     [HttpPost]
-   //     public void Post([FromBody]string value)
-   //     {
-			//return database.VenueAdd(["Vname"].value, ["Address"].value, )
-   //     }
+		}
 
-   //     // PUT api/Venues/5
-   //     [HttpPut("{id}")]
-   //     public void Put(int id, [FromBody]string value)
-   //     {
-   //     }
 
-   //     // DELETE api/Venues/5
-   //     [HttpDelete("{id}")]
-   //     public void Delete(int id)
-   //     {
-   //     }
-    }
+
+
+		//     // POST api/Venues
+		//     [HttpPost]
+		//     public void Post([FromBody]string value)
+		//     {
+		//return database.VenueAdd(["Vname"].value, ["Address"].value, )
+		//     }
+
+		//     // PUT api/Venues/5
+		//     [HttpPut("{id}")]
+		//     public void Put(int id, [FromBody]string value)
+		//     {
+		//     }
+
+		//     // DELETE api/Venues/5
+		//     [HttpDelete("{id}")]
+		//     public void Delete(int id)
+		//     {
+		//     }
+	}
 }
