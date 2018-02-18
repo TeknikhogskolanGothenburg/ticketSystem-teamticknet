359aad9b50b2bff874d4285bb1f6df0b0e502321
Grasuggan <32665572+Grasuggan@users.noreply.github.com>
2018-02-18 14:18:44 +0100

    Edited AllInfoByDateController

--- src/Restapi_Webapplication/Controllers/AllInfoByDateController.cs
+++ src/Restapi_Webapplication/Controllers/AllInfoByDateController.cs
@@ -2,16 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using ClassLibraryTicketShop;
+using Microsoft.AspNetCore.Http;
 using TicketSystem.DatabaseRepository;
+using ClassLibraryTicketShop;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Restapi_Webapplication.Controllers
 {
     [Produces("application/json")]
     [Route("api/AllInfoByDate")]
-    public class AllInfoByDateController : Controller
+    public class AllInfoByDateController : ControllerBase
     {
 		TicketDatabase tbd = new TicketDatabase();
 
