615b8bca9eddb95585bdf1288cab8c9a3cb5ed6e
Grasuggan <32665572+Grasuggan@users.noreply.github.com>
2018-03-03 11:15:19 +0100

    Added Required attribute to our new class in classLibraryTicketShop  AllEventsByDate

--- src/ClassLibraryTicketShop/AllEventsByDate.cs
+++ src/ClassLibraryTicketShop/AllEventsByDate.cs
@@ -1,17 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace ClassLibraryTicketShop
 {
     public class AllEventsByDate
     {
+		[Required]
 		public DateTime EventStartDateTime { get; set; }
+		[Required]
 		public string EventName { get; set; }
+		[Required]
 		public string EventHtmlDescription { get; set; }
+		[Required]
 		public string VenueName { get; set; }
+		[Required]
 		public string Address { get; set; }
+		[Required]
 		public string City { get; set; }
+		[Required]
 		public string Country { get; set; }
 	}
 }
